31a8d665a1b9509082a478121762b6f84b39cccb
Ani Loprais <an.loprais@gmail.com>
2023-02-21 09:06:51 +0100

    Vytvoření mincí pro kávovar

--- Coffee_machine/main.py
+++ Coffee_machine/main.py
@@ -11,7 +11,17 @@ def report(data):
     print(f"Coffee: {data['coffee']}")
 
 def coins():
-    print("Prosím vložte mince 1, 2, 5, 10, 20, 50")
+    print("Please insert coins 1, 2, 5, 10, 20, 50")
+    kc1 = int(input("How many CZK 1 do you want to deposit? "))
+    kc2 = int(input("How many CZK 2 do you want to deposit? ")) * 2
+    kc5 = int(input("How many CZK 5 do you want to deposit? ")) * 5
+    kc10 = int(input("How many CZK 10 do you want to deposit? ")) * 10
+    kc20 = int(input("How many CZK 20 do you want to deposit? ")) * 20
+    kc50 = int(input("How many CZK 50 do you want to deposit? ")) * 50
+    amount = kc1 + kc2 + kc5 + kc10 + kc20 +kc50
+    print(f"Total insert amount: CZK {amount}")
+    return amount
+
 
 # >>> Machine code <<<
 user_choice = input("Co byste dal/a? (espresso/latte/cappuccino): ")
