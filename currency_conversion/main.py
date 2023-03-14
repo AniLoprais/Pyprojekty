d634362e8e7c3ed090245b40206c4c37e4e738c1
Ani Loprais <an.loprais@gmail.com>
2023-03-14 12:17:13 +0100

    Creating the second window in tkinker

--- currency_conversion/main.py
+++ currency_conversion/main.py
@@ -4,11 +4,16 @@ import tkinter
 window = tkinter.Tk()
 window.title("Moje první okno")
 window.minsize(width=500, height=400)
-window.resizable(True, True)
+window.resizable(False, False)
 window.iconbitmap("icon.png")
-window.config(bg="#1b073c")
+window.config(bg="#5b0080")
 
-
+window2 = tkinter.Tk()
+window2.title("Moje druhé okno")
+# window2.minsize(width=300, height=350)
+window2.geometry("300x400+800+100")
+window2.resizable(False, False)
+window2.config(bg="#58d330")
 
 
 
